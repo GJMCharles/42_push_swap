1238b8f95ba6ac57d8e016d0382830f3323688c0
GJMCharles <91600327+GJMCharles@users.noreply.github.com>
2025-10-20 23:55:30 +0200

    main function

--- push_swap.c
+++ push_swap.c
@@ -10,4 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+int	main(int argc, char *argv)
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+}
